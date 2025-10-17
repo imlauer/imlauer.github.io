a121e512ba07aa954b6611ab2b82cfee390e2031
Andrés I. <suurrend3r@gmail.com>
2025-10-17 16:47:31 -0300

    index

--- up.sh
+++ up.sh
@@ -1,1 +1,15 @@
-rm index.html && cmark index.md > index.html && git add . && git commit -m index && git push
+#!/usr/bin/bash
+rm index.html  &&
+(
+echo '<!DOCTYPE html>'
+echo '<html lang="en">'
+echo '<head>'
+echo '  <meta charset="UTF-8">'
+echo '  <title>Imlauer</title>'
+echo '</head>'
+echo '<body>'
+cmark index.md
+echo '</body>'
+echo '</html>'
+) | tee index.html &&
+git add . && git commit -m index && git push
