68f9d381e8b61a834b5505a89129a8f45e5381b3
Andrés I. <suurrend3r@gmail.com>
2025-11-04 06:24:44 -0300

    music

--- up.sh
+++ up.sh
@@ -6,25 +6,25 @@ exit;
 fi
 
 for html in *.html; do 
-	rm $html;
+	cp $html neocites;
 done
-for markdown in *.md; do 
-	filename_only="${markdown%.*}"
-	#(
-	#echo '<!DOCTYPE html>'
-	#echo '<html lang="en">'
-	#echo '<head>'
-	#echo '  <meta charset="UTF-8">'
-	#echo "  <title>Imlauer | $filename_only </title>"
-	#echo '</head>'
-	#echo '<body>'
-	#cmark $markdown;
-	#echo '</body>'
-	#echo '</html>'
-	#) | tee $filename_only.html &&
-	#cp $filename_only.html neocities/ 
-	pandoc -s $markdown -c style.css | tee $filename_only.html
-	cp $filename_only.html neocities/
-done ;
+#for markdown in *.md; do 
+#	filename_only="${markdown%.*}"
+#	#(
+#	#echo '<!DOCTYPE html>'
+#	#echo '<html lang="en">'
+#	#echo '<head>'
+#	#echo '  <meta charset="UTF-8">'
+#	#echo "  <title>Imlauer | $filename_only </title>"
+#	#echo '</head>'
+#	#echo '<body>'
+#	#cmark $markdown;
+#	#echo '</body>'
+#	#echo '</html>'
+#	#) | tee $filename_only.html &&
+#	#cp $filename_only.html neocities/ 
+#	pandoc -s $markdown -c style.css | tee $filename_only.html
+#	cp $filename_only.html neocities/
+#done ;
 
 git add . && git commit -m $1 && git push
